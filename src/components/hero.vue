fcfcad8a8bb2866a924f57f375eaf239152820e1
dvdptr <info@davidebner.de>
2020-02-12 16:21:51 +0100

    hero comp

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -1,25 +1,6 @@
 <template>
   <div class="mx-auto pt-4 pb-12 lg:pt-16 lg:pb-24">
     <p
-<<<<<<< HEAD
-      class="md:w-3/4 w-full text-center mx-auto text-gray-800 py-2 text-2xl md:text-4xl leading-tight font-bold font-test"
-    >A market as open as art itself</p>
-
-    <p class="w-full md:w-1/2 mx-auto pt-8 text-gray-800 text-center md:text-left">
-      We are building a
-      <span class="highlight-text">decentralized application</span>
-      focusing on trustless interaction between artists, art enthusiasts,
-      museums & collectors.
-    </p>
-    <p class="w-full md:w-1/2 mx-auto text-gray-800 text-center md:text-left pt-4">
-      By rethinking the structure of ownership and
-      value distribution, we adopt web3 standards to the art market.
-    </p>
-
-    <div
-      class="w-full md:w-1/2 pt-8 mx-auto text-gray-800 text-center md:text-left sm:inline md:hidden"
-    >coming soon</div>
-=======
       class="w-11/12 lg:w-1/2 text-center mx-auto py-2 text-4xl lg:text-6xl leading-tight font-black fontManrope"
     >A market as open as art itself</p>
 
@@ -36,26 +17,15 @@
         value distribution, we adopt web3 standards to the art market.
       </p>
     </div>
->>>>>>> update
   </div>
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
-  name: "hero"
-=======
   name: "Hero"
->>>>>>> update
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-<<<<<<< HEAD
-.highlight-text {
-  @apply bg-gray-200 p-1 font-bold rounded-lg;
-}
-=======
->>>>>>> update
 </style>
