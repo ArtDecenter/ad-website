c3c8d8075a7ceef80ca5f019c407f8db1f6d21bb
dvdptr <info@davidebner.de>
2020-02-12 09:30:25 +0100

    minor updates

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="p-6 md:p-12 mx-auto">
     <p
-          class="md:w-3/4 w-full text-center mx-auto text-gray-800 py-2 text-2xl md:text-4xl leading-tight font-bold"
-        >
-          A market as open as art itself
-        </p>
+      class="md:w-3/4 w-full text-center mx-auto text-gray-800 py-2 text-2xl md:text-4xl leading-tight font-bold font-test"
+    >A market as open as art itself</p>
 
-        <p class="w-full md:w-1/2 mx-auto pt-8 text-gray-800 text-center md:text-left">
-          We are building a
-          <span class="highlight-text">decentralized application</span>
-          focusing on trustless interaction between artists, art enthusiasts,
-          museums & collectors.
-        </p>
-        <p class="w-full md:w-1/2 mx-auto text-gray-800 text-center md:text-left pt-4">By rethinking the structure of ownership and
-          value distribution, we adopt web3 standards to the art market.</p>
+    <p class="w-full md:w-1/2 mx-auto pt-8 text-gray-800 text-center md:text-left">
+      We are building a
+      <span class="highlight-text">decentralized application</span>
+      focusing on trustless interaction between artists, art enthusiasts,
+      museums & collectors.
+    </p>
+    <p class="w-full md:w-1/2 mx-auto text-gray-800 text-center md:text-left pt-4">
+      By rethinking the structure of ownership and
+      value distribution, we adopt web3 standards to the art market.
+    </p>
 
-          <div class="w-full md:w-1/2 pt-8 mx-auto text-gray-800 text-center md:text-left sm:inline md:hidden">coming soon</div>
-   
-    
+    <div
+      class="w-full md:w-1/2 pt-8 mx-auto text-gray-800 text-center md:text-left sm:inline md:hidden"
+    >coming soon</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hero',
-  
-}
+  name: "hero"
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.highlight-text{
-  @apply bg-gray-200 p-1 font-bold rounded-lg
+.highlight-text {
+  @apply bg-gray-200 p-1 font-bold rounded-lg;
 }
 </style>
